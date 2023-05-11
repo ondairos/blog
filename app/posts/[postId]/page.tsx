b8156a1f7837efd598b36b75e845f000eac8940d
Ondairos <ondairos@gmail.com>
2023-05-11 11:06:39 +0300

    added Remark for MD contet read and Post Comp

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,8 +1,11 @@
-import { getSortedPostsData } from "@/lib/posts";
+import { getPostData, getSortedPostsData } from "@/lib/posts";
 import React from "react";
 import { notFound } from "next/navigation";
+import getFormattedDate from "@/lib/getFormattedDate";
+import Link from "next/link";
 
 export function generateMetadata({ params }: { params: { postId: string } }) {
+  // getSortedPostsData from lib logic file
   const posts = getSortedPostsData(); //deduped request we already got the data!
   const { postId } = params;
 
@@ -29,10 +32,20 @@ export default async function Post({ params }: { params: { postId: string } }) {
   if (!posts.find((post) => post.id === postId)) {
     return notFound();
   }
+  // getPostData from lib logic file
+  const { title, date, contentHtml } = await getPostData(postId);
+  const publishedDate = getFormattedDate(date);
 
   return (
-    <div>
-      <p></p>
-    </div>
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+      <p className="mt-0">{publishedDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }}></section>
+        <p>
+          <Link href="/">Back to Home</Link>
+        </p>
+      </article>
+    </main>
   );
 }
