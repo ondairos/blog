d336f61d23ef553e1948662bc675bf5d9e02b4a8
Ondairos <ondairos@gmail.com>
2023-05-17 16:19:52 +0300

    wrong changes

---
+++ app/api/posts.ts
@@ -0,0 +1,36 @@
+import fs from "fs";
+import path from "path";
+import matter, { GrayMatterFile } from "gray-matter";
+
+interface BlogPost {
+    id: string;
+    title: string;
+    date: string;
+}
+
+const postsDirectory = path.join(process.cwd(), "blogposts");
+
+export default function handler(req: any, res: any) {
+    const fileNames = fs.readdirSync(postsDirectory);
+
+    const allPostsData = fileNames.map((fileName) => {
+        const id = fileName.replace(/\.md$/, "");
+        const fullPath = path.join(postsDirectory, fileName);
+        const fileContents = fs.readFileSync(fullPath, "utf8");
+        const matterResult: GrayMatterFile<string> = matter(fileContents);
+
+        const blogPost: BlogPost = {
+            id,
+            title: matterResult.data.title,
+            date: matterResult.data.date,
+        };
+
+        return blogPost;
+    });
+
+    const sortedPostsData = allPostsData.sort((a, b) =>
+        a.date < b.date ? 1 : -1
+    );
+
+    res.status(200).json(sortedPostsData);
+}
--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,18 +1,44 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { SyntheticEvent, useEffect, useState } from "react";
 import { getSortedPostsData } from "@/lib/posts";
 import ListItem from "./ListItem";
 
 export default function Posts() {
+  const [posts, setPosts] = useState<BlogPost[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
   // getting right away, not gonna use async await
-  const posts = getSortedPostsData();
+  // const posts = getSortedPostsData();
+
+  useEffect(() => {
+    const fetchPostsData = async () => {
+      const data = await getSortedPostsData();
+      setPosts(data);
+    };
+
+    fetchPostsData();
+  }, []);
+
+  const filteredPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handeSearchChange = (event: any) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <section className="mt-6 mx-auto max-2-2xl">
       <h2 className="text-4xl font-bold dark:text-white/90">Posts</h2>
+      <input
+        type="text"
+        placeholder="Search posts"
+        value={searchQuery}
+        onChange={handeSearchChange}
+        className="mt-4 px-2 py-1 border border-gray-300 rounded"
+      />
       <ul className="w-full">
-        {posts.map((post) => (
+        {filteredPosts.map((post) => (
           <ListItem key={post.id} post={post} />
         ))}
       </ul>
