068e29d4eb9f11de9d5480eb2ea92287f77b6e97
Ondairos <ondairos@gmail.com>
2023-05-26 12:28:03 +0300

    added dateString fix and Search functionality

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,15 +1,46 @@
-import React from "react";
-import { getSortedPostsData } from "@/lib/posts";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import ListItem from "./ListItem";
 
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   // getting right away, not gonna use async await
-  const posts = getSortedPostsData();
+  // const posts = getSortedPostsData();
+  useEffect(() => {
+    fetchPostsData();
+  }, []);
+
+  const fetchPostsData = async () => {
+    const response = await fetch("/api/posts");
+    const data = await response.json();
+    setPosts(data);
+  };
+
+  const filteredPosts = posts.filter((post: BlogPost) =>
+    post.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // change
+  const handleSearchChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <section className="mt-6 mx-auto max-2-2xl">
       <h2 className="text-4xl font-bold dark:text-white/90">Posts</h2>
+      <div className="my-4">
+        <input
+          type="text"
+          placeholder="Search posts by title"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className="border border-gray-300 px-4 py-2 rounded-md w-60"
+        />
+      </div>
       <ul className="w-full">
-        {posts.map((post) => (
+        {filteredPosts.map((post: BlogPost) => (
           <ListItem key={post.id} post={post} />
         ))}
       </ul>
